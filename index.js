ec92bc6722a7e8c68e40f0acedaa7fd4c7635c68
OG-Gardavsky <ogardavsky@gmail.com>
2022-07-10 16:00:14 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const puppeteer = require('puppeteer');
+
+
+const goToNextPage = async(page) => {
+    const nextButton = (await page.$x('/html/body/div[2]/div[1]/div[2]/div[3]/div[3]/div/div/div/div/div[3]/div/div[22]/ul[1]/li[12]/a'))[0]
+    nextButton.click()
+}
+
+const run = async () => {
+    const browser = await puppeteer.launch({
+        headless: false,    
+    });
+    const page = await browser.newPage()
+    // await page.goto('https://www.sreality.cz/')
+
+    // await page.waitForTimeout(5000)
+    // console.log('waited')
+
+
+    //switch language
+    // const czechButton = (await page.$x('/html/body/div[2]/preact[1]/div/header/div[3]/div[1]/div/button'))[0]
+    // czechButton.click()
+    // const englishButton = (await page.$x('/html/body/div[2]/preact[1]/nav/ul/li[1]/a'))[0]
+    // englishButton.click()
+
+    
+    await page.goto('https://www.sreality.cz/en/search/for-sale/apartments?order=cheapest')
+    await page.waitForTimeout(5000)
+    console.log('waited')
+
+    await goToNextPage(page)
+
+
+    
+
+
+
+
+
+
+
+
+    await page.waitForTimeout(1000)
+
+
+   
+    return 'done'
+}
+
+
+run()
+    .then((res)=> console.log(res))
