3a2e1c90941538a1fd2865f28c52faba591c1fac
OG-Gardavsky <ogardavsky@gmail.com>
2022-07-17 18:48:09 +0200

    switching to page by using url params

--- src/webscraping/index.js
+++ src/webscraping/index.js
@@ -2,20 +2,19 @@ const puppeteer = require('puppeteer')
 const fs = require('fs')
 
 
-const goToNextPage = async(page) => {
-    const nextButton = (await page.$x('/html/body/div[2]/div[1]/div[2]/div[3]/div[3]/div/div/div/div/div[3]/div/div[22]/ul[1]/li[12]/a'))[0]
-    nextButton.click()
+const goToSrealityPage = async(page, pageNumber) => {
+    await page.goto(`https://www.sreality.cz/en/search/for-sale/apartments?page=${pageNumber}&order=cheapest`)
 }
 
 const getEstateXpath = (index) => {
     return `//*[@id="page-layout"]/div[2]/div[3]/div[3]/div/div/div/div/div[3]/div/div[${index}]`
 }
 
-const scan1page = async(page, numberOfEstates) => {
+const scanOnepage = async(page, numberOfEstates) => {
     const realEstates = []
     await page.waitForXPath(getEstateXpath(1));
 
-    for(let i = 1; i <= 20; i++) {
+    for(let i = 1; i <= numberOfEstates; i++) {
         const baseXpath = getEstateXpath(i)
         const elTitle = await page.$x(`${baseXpath}//span[@class="name ng-binding"]`)
         const title  = await page.evaluate(el => el.textContent, elTitle[0])
@@ -36,22 +35,21 @@ const scan1page = async(page, numberOfEstates) => {
     return realEstates
 }
 
+
 const run = async () => {
     const browser = await puppeteer.launch({
         headless: false,    
     });
     const page = await browser.newPage()
     
-    
-    await page.goto('https://www.sreality.cz/en/search/for-sale/apartments?order=cheapest')
-    await page.waitForTimeout(3000)
 
     let realEstatesList = []
     for(let i = 0; i < 10; i++) {
-        const onePageEstates = await scan1page(page, 20)
-        realEstatesList = [...realEstatesList, ...onePageEstates]
-        await goToNextPage(page)
-        await page.waitForTimeout(2000)
+        await goToSrealityPage(page, i+1)
+        await page.waitForTimeout(4000)
+        
+        const onePageEstates = await scanOnepage(page, 20)
+        realEstatesList = [...realEstatesList, ...onePageEstates] 
     }
 
 
