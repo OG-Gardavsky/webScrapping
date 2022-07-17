7709d3695cd872a27f931da8987f17cc0c9b6383
OG-Gardavsky <ogardavsky@gmail.com>
2022-07-17 17:42:48 +0200

    getting 200 real estates

--- index.js
+++ index.js
@@ -6,6 +6,38 @@ const goToNextPage = async(page) => {
     nextButton.click()
 }
 
+const getEstateXpath = (index) => {
+    return `//*[@id="page-layout"]/div[2]/div[3]/div[3]/div/div/div/div/div[3]/div/div[${index}]`
+}
+
+const scan1page = async(page, numberOfEstates) => {
+    const realEstates = []
+
+    await page.waitForXPath(getEstateXpath(1));
+
+
+    for(let i = 1; i <= 20; i++) {
+        const baseXpath = getEstateXpath(i)
+        const elTitle = await page.$x(`${baseXpath}//span[@class="name ng-binding"]`)
+        const title  = await page.evaluate(el => el.textContent, elTitle[0])
+
+        const elImage = await page.$x(`${baseXpath}//img`)
+        const imageLink  = await page.evaluate(el => el.getAttribute('src'), elImage[0])
+
+        const elUrlParams = await page.$x(`${baseXpath}//a[@ng-href]`)
+        const urlParams  = await page.evaluate(el => el.getAttribute('ng-href'), elUrlParams[0])
+        const url = `https://www.sreality.cz/${urlParams}`
+
+        realEstates.push({
+            title,
+            imageLink,
+            url
+        })
+    }
+
+    return realEstates
+}
+
 const run = async () => {
     const browser = await puppeteer.launch({
         headless: false,    
@@ -17,22 +49,21 @@ const run = async () => {
     await page.goto('https://www.sreality.cz/en/search/for-sale/apartments?order=cheapest')
     await page.waitForTimeout(3000)
 
+    let realEstatesList = []
+    for(let i = 0; i < 10; i++) {
+        const onePageEstates = await scan1page(page, 20)
+        realEstatesList = [...realEstatesList, ...onePageEstates]
+        await goToNextPage(page)
+        // await page.waitForTimeout(1000)
+    }
 
-
-
-    const estateTitles = await page.$$eval('.name.ng-binding', elements => {
-        return elements.map(element => element.innerText)
-    })
-
-    console.log(estateTitles)
+    console.log(realEstatesList.length)
     
-    
-    
+
  
 
-
-    await goToNextPage(page)
-    await page.waitForTimeout(1000)
+    
+    // await page.waitForTimeout(1000)
     browser.close()
     return 'done'
 }
