0006b3ef66762bd7f120d7a2de796da0abe54027
OG-Gardavsky <ogardavsky@gmail.com>
2022-07-17 19:00:21 +0200

    check of uniqueness of values

---
+++ src/webscraping/checkUniquenes.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+
+
+const estatesBuff = fs.readFileSync('results.json')
+const estates = JSON.parse(estatesBuff.toString())
+const urls =  estates.map(estate => estate.url)
+
+
+
+function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index
+}
+const unique = urls.filter(onlyUnique)
+
+console.log('is same length' , unique.length === urls.length1)
+
