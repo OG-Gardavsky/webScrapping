7c7dc85cc0bebd24e2cb52653e1bdc171a69b753
OG-Gardavsky <ogardavsky@gmail.com>
2022-08-11 23:22:28 +0200

    webscraping split into utils

--- webscrapping/src/index.js
+++ webscrapping/src/index.js
@@ -1,76 +1,23 @@
 const puppeteer = require('puppeteer')
 const fs = require('fs')
+const { createStructure } = require('./db')
+const {browserConfig, scrapeEstates} = require("./utils");
 
 
-const goToSrealityPage = async(page, pageNumber) => {
-    await page.goto(`https://www.sreality.cz/en/search/for-sale/apartments?page=${pageNumber}&order=cheapest`)
-}
-
-const getEstateXpath = (index) => {
-    return `//*[@id="page-layout"]/div[2]/div[3]/div[3]/div/div/div/div/div[3]/div/div[${index}]`
-}
-
-const scanOnepage = async(page, numberOfEstates, numberOfImages) => {
-    const realEstates = []
-    await page.waitForXPath(getEstateXpath(1));
-
-    for(let i = 1; i <= numberOfEstates; i++) {
-        const baseXpath = getEstateXpath(i)
-        const elTitle = await page.$x(`${baseXpath}//span[@class="name ng-binding"]`)
-        const title  = await page.evaluate(el => el.textContent, elTitle[0])
-
-        const imageLinks = []
-        for(let i = 1; i < numberOfImages + 1; i++) {
-            const elImage = await page.$x(`${baseXpath}//a[${i}]/img`)
-            const imageLink  = await page.evaluate(el => el.getAttribute('src'), elImage[0])
-            imageLinks.push(imageLink)
-        }
-        
-
-        const elUrlParams = await page.$x(`${baseXpath}//a[@ng-href]`)
-        const urlParams  = await page.evaluate(el => el.getAttribute('ng-href'), elUrlParams[0])
-        const url = `https://www.sreality.cz/${urlParams}`
-
-        realEstates.push({
-            title,
-            imageLinks,
-            url
-        })
-    }
-    return realEstates
-}
 
 
 const run = async () => {
-    const browser = await puppeteer.launch({
-        headless: true,
-        executablePath: '/usr/bin/chromium-browser',
-        args: [
-          '--no-sandbox',
-          '--disable-gpu',
-        ]
-    })
-    const page = await browser.newPage()
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage(browserConfig)
     
-    const realEstatesList = []
-    for(let i = 0; i < 10; i++) {
-        await goToSrealityPage(page, i+1)
-        
-        const onePageEstates = await scanOnepage(page, 20, 3)
-        realEstatesList.push(...onePageEstates)
-    }
+    // const realEstatesList = await scrapeEstates(page, 10, 20, 3)
 
 
-    fs.writeFile('results.json', JSON.stringify(realEstatesList), function (err,data) {
-        if (err) {
-            return console.log(err)
-        }
-        console.log(data)
-    })
+    await createStructure()
     
     
     await page.waitForTimeout(1000)
-    browser.close()
+    await browser.close()
     return 'done'
 }
 
---
+++ webscrapping/src/utils.js
@@ -0,0 +1,59 @@
+const goToSrealityPage = async(page, pageNumber) => {
+    await page.goto(`https://www.sreality.cz/en/search/for-sale/apartments?page=${pageNumber}&order=cheapest`)
+}
+
+const getEstateXpath = (index) => {
+    return `//*[@id="page-layout"]/div[2]/div[3]/div[3]/div/div/div/div/div[3]/div/div[${index}]`
+}
+
+const scanOnepage = async(page, numberOfEstates, numberOfImages) => {
+    const realEstates = []
+    await page.waitForXPath(getEstateXpath(1));
+
+    for(let i = 1; i <= numberOfEstates; i++) {
+        const baseXpath = getEstateXpath(i)
+        const elTitle = await page.$x(`${baseXpath}//span[@class="name ng-binding"]`)
+        const title  = await page.evaluate(el => el.textContent, elTitle[0])
+
+        const imageLinks = []
+        for(let i = 1; i < numberOfImages + 1; i++) {
+            const elImage = await page.$x(`${baseXpath}//a[${i}]/img`)
+            const imageLink  = await page.evaluate(el => el.getAttribute('src'), elImage[0])
+            imageLinks.push(imageLink)
+        }
+
+
+        const elUrlParams = await page.$x(`${baseXpath}//a[@ng-href]`)
+        const urlParams  = await page.evaluate(el => el.getAttribute('ng-href'), elUrlParams[0])
+        const url = `https://www.sreality.cz/${urlParams}`
+
+        realEstates.push({
+            title,
+            imageLinks,
+            url
+        })
+    }
+    return realEstates
+}
+
+const scrapeEstates =  async (page, numberOfPages, numberOfEstatesOnPage, numberOfImages) => {
+    const realEstatesList = []
+    for(let i = 0; i < numberOfPages; i++) {
+        await goToSrealityPage(page, i+1)
+
+        const onePageEstates = await scanOnepage(page, numberOfEstatesOnPage, numberOfImages)
+        realEstatesList.push(...onePageEstates)
+    }
+    return realEstatesList
+}
+
+const browserConfig  = {
+    headless: true,
+    executablePath: '/usr/bin/chromium-browser',
+    args: [
+        '--no-sandbox',
+        '--disable-gpu',
+    ]
+}
+
+module.exports = { goToSrealityPage, getEstateXpath, scanOnepage, scrapeEstates, browserConfig, }
