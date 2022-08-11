44be94e2e7c054d829846c82dbfc4f0720dd5aa6
OG-Gardavsky <ogardavsky@gmail.com>
2022-08-11 23:22:42 +0200

    added db util webscraping

---
+++ webscrapping/src/db.js
@@ -0,0 +1,50 @@
+const { Client } = require("pg")
+const dotenv = require("dotenv")
+dotenv.config()
+
+
+const connectDb = async () => {
+    try {
+        const client = new Client({
+            user: process.env.PGUSER,
+            host: process.env.PGHOST,
+            database: process.env.PGDATABASE,
+            password: process.env.PGPASSWORD,
+            port: process.env.PGPORT
+        })
+
+        await client.connect()
+        
+        return client
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+
+
+const createStructure = async () => {
+    const client = connectDb()
+
+    const text = `
+        CREATE TABLE IF NOT EXISTS "estate" (
+            "id" SERIAL,
+            "title" VARCHAR(100),
+            "url" VARCHAR(100),
+            "image_url" VARCHAR(100),
+            PRIMARY KEY ("id")
+        );`
+
+    try {
+        await client.query(text)
+        await client.end()
+    } catch (e) {
+        console.log(e)
+    }
+    
+    
+}
+
+
+
+module.exports = { connectDb, createStructure }
