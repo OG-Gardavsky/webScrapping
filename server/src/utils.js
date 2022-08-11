1389ad88a68a4c5eae550f47a0d13b34b6055433
OG-Gardavsky <ogardavsky@gmail.com>
2022-08-11 13:57:56 +0200

    db create table

--- server/src/utils.js
+++ server/src/utils.js
@@ -3,6 +3,38 @@ const dotenv = require("dotenv")
 dotenv.config()
 
 
-connectDb()
+const connectDb = async () => {
+    try {
+        const client = new Client({
+            user: process.env.PGUSER,
+            host: process.env.PGHOST,
+            database: process.env.PGDATABASE,
+            password: process.env.PGPASSWORD,
+            port: process.env.PGPORT
+        })
+
+
+
+        await client.connect()
+        console.log('conected')
+
+
+        const text = `
+            CREATE TABLE IF NOT EXISTS "estate" (
+                "id" SERIAL,
+                "title" VARCHAR(100) NOT NULL,
+                "url" VARCHAR(100) NOT NULL,
+                PRIMARY KEY ("id")
+            );`
+
+
+        const res = await client.query(text)
+
+
+        await client.end()
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 module.exports = { connectDb }
