93406d896dcb4d6b3526fe8f511decfa9a653442
OG-Gardavsky <ogardavsky@gmail.com>
2022-08-14 11:31:05 +0200

    pagination POC

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -4,22 +4,20 @@ import Header from 'components/Header';
 import ApartmentCard from 'components/ApartmentCard';
 import {useEffect, useState} from "react";
 import Card from '../components/Card'
+import Pagination from "../components/Pagination";
 
 export default function HomePage() {
 
     const [estatelist, setEstatelist] = useState([])
     const [loading, setLoading] = useState(true)
     const [loadError, setLoadError] = useState(false)
+    const [currentPage, setCurrentPage] = useState(1)
+    const [totalPosts, setTotalPosts] = useState(200)
 
-    const getEstates = async () => {
 
-        const res = await fetch('http://localhost:8080/estates')
-        const { estates } = await res.json()
-        setEstatelist(estates)
-    }
 
     useEffect(() => {
-        getEstates()
+        getEstates(currentPage)
             .then(() => {
                 setLoading(false)
                 setLoadError(false)
@@ -27,6 +25,17 @@ export default function HomePage() {
             .catch((err) => setLoadError(true))
 
     }, [])
+
+    const getEstates = async (pageNum) => {
+        const res = await fetch(`http://localhost:8080/estates?page=${pageNum}`)
+        const { estates } = await res.json()
+        setEstatelist(estates)
+    }
+
+    const paginate = async (number) => {
+        setCurrentPage(number)
+        await getEstates(number)
+    }
 
     return (
         <>
@@ -36,6 +45,7 @@ export default function HomePage() {
             <main>
                 <Header />
 
+                <Card><Pagination currentPage={currentPage} postsPerPage={20} totalPosts={totalPosts} paginate={paginate}/></Card>
                 { loadError && <Card><h1>Sorry, we are unable to load estates.</h1></Card> }
                 { loading && <Card><h1>Loading....</h1></Card> }
 
